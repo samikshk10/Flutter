860d22e68ef2f977df81a9da54681fc083e7c86d
samikshk10 <samik.shakya8848@gmail.com>
2024-03-07 10:25:48 +0545

    some changes

--- lab1/lib/main.dart
+++ lab1/lib/main.dart
@@ -40,7 +40,6 @@ void main() {
                           Icons.library_books,
                           color: Colors.white,
                         ),
-                        SizedBox(height: 8.0),
                         Text(
                           'Book: 450',
                           style: TextStyle(color: Colors.white, fontSize: 12.0),
@@ -59,7 +58,6 @@ void main() {
                           Icons.account_circle,
                           color: Colors.white,
                         ),
-                        SizedBox(height: 8.0),
                         Text(
                           'View Profile',
                           style: TextStyle(color: Colors.white, fontSize: 12.0),
