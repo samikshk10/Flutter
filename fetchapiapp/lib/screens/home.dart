dfb4b1bdf45d18ff0874f8069a963f0f6daedc26
samikshk10 <samik.ytd@gmail.com>
2024-03-29 12:11:34 +0545

    flutter create fetch api

--- fetchapiapp/lib/screens/home.dart
+++ fetchapiapp/lib/screens/home.dart
@@ -120,7 +120,7 @@ class AgentDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(15.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
